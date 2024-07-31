498a651cff4a42ce8bb33dd50a2f566f779c3c83
devgabo <gabopinedasand@gmail.com>
2024-07-31 13:36:49 -0500

    Primera Clase

---
+++ chart/charts.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as a
+
+def generate_pie_chart():
+    labels = ['A','B','C']
+    values = [200,34, 120]
+
+    #fig, ax = plt.subplot()
+    #ax.pie(values,labels)
+    #plt.savefig('pie.png')
+    #plt.close()
+
+    fig, ax = a.subplots()
+    ax.pie(values,labels=labels)
+    a.savefig('pie.png')
+    a.close()
+    #pylot.show()
---
+++ main.py
@@ -0,0 +1,8 @@
+import chart.charts
+
+def run():
+    chart.charts.generate_pie_chart()
+    print ("Hola Mundo")
+
+if __name__ == "__main__":
+    run()
